68cad6c4db00e0b6cf3a58c1230f5a2b1cf5412b
David Graham <ce-dggraham9721@st.umat.edu.gh>
2024-05-17 23:32:18 +0000

    Bot Updated 3

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -1,13 +1,18 @@
+# All the Libraries needed for this project
 from typing import Final
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 
-TOKEN: Final = 'YOUR BOT TOKEN'
-BOT_USERNAME : Final = '@CTSL_Bot'
+TOKEN: Final = 'YOUR BOT TOKEN GOES HERE'
+BOT_USERNAME : Final = '@CTSL_Bot' # Your BOT USERNAME & should start with the @ symbol
 
 
-#Commands
+#Commands the user can intereact with on telegram
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Welcome To The CTSL WORK Assistant Bot")
+    
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! Welcome To The Help Desk of CTSL")
+  
   
