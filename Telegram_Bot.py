b38fcb650bf9d6b8d1b82184fe19ed81396d3322
David Graham <ce-dggraham9721@st.umat.edu.gh>
2024-05-17 23:23:42 +0000

    Bot_Updated

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -1,4 +1,13 @@
+from typing import Final
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
+TOKEN: Final = '717mat1122307:zAFv0APwm232afsuUghH7TfUkcanzPgJpuloops'
+BOT_USERNAME : Final = '@CTSL_Bot'
+
+
+#Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! Welcome To The CTSL WORK Assistant Bot")
+  
